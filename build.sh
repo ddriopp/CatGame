382e950fcce8011d4ecde9459bdde61476f68863
Matthew Li <lignay@me.com>
2021-08-11 17:44:14 +0800

    Fixed local path to download go lang pkg

--- build.sh
+++ build.sh
@@ -3,14 +3,15 @@
 BUILD_WKS="/home/liyang/stage/actions-runner/demo_work/CatGame/CatGame"
 GO_PKG_NAME="go1.16.7.linux-amd64.tar.gz"
 GO_PKG_URL="https://golang.org/dl/$GO_PKG_NAME"
+STAGE_WKS="$HOME/stage"
 
 echo "build is in porgress ..."
 
 if ! go version >/dev/null 2>&1; then
-  wget $GO_PKG_URL -O $GO_PKG_NAME
-  tar -C /usr/local -xzf $GO_PKG_NAME
+  wget -nv $GO_PKG_URL -O $STAGE_WKS/$GO_PKG_NAME
+  tar -C /usr/local -xzf $STAGE_WKS/$GO_PKG_NAME
   if ! grep '/usr/local/go/bin' "$HOME/.bash_profile" >/dev/null; then
-    cat >> $HOME/.bash_profile << EOF
+    cat >> $HOME/.bash_profile << 'EOF'
 export PATH=$PATH:/usr/local/go/bin
 EOF
   fi  
